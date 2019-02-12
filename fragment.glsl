38c0672e4def0a66973a74e0a1bae0c1f70d640f
Amanda Cao <acao004@wch136-08.cs.ucr.edu>
2019-02-11 22:55:22 -0800

    matched final result

--- fragment.glsl
+++ fragment.glsl
@@ -10,7 +10,8 @@ void main()
     vec4 e_norm = normalize(-position);
 
 
-    vec4 ambient = gl_LightModel.ambient; 
+    //vec4 ambient = gl_LightModel.ambient;
+    vec4 ambient = vec4(0,0,0,0);
     vec4 diffuse = vec4(0, 0, 0, 0);
     vec4 specular = vec4(0, 0, 0, 0);
 
@@ -19,7 +20,7 @@ void main()
 
 	ambient += gl_FrontMaterial.ambient * gl_LightSource[i].ambient;
 	diffuse += gl_LightSource[i].diffuse * gl_FrontMaterial.diffuse * max(dot(n, normalize(l)), float(0));
-	specular += gl_LightSource[i].specular * gl_FrontMaterial.specular * pow(max(dot(e_norm, normalize(reflection)), float(0)), gl_FrontMaterial.shininess);
+	specular += gl_LightSource[i].specular * gl_FrontMaterial.specular * pow(max(dot(e_norm, reflection), float(0)), gl_FrontMaterial.shininess);
     }
 
     gl_FragColor = ambient + diffuse + specular;
